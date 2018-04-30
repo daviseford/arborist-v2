b9e1bafe5821b696063b36774d3351994d8bd28e
Davis Ford <ford_davis@bah.com>
2018-04-29 23:04:30 -0400

    added arborist icon!

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import ButtonLinkTo from '../ui/ButtonLinkTo';
 import { kAppName, kRoutes, kVersion } from '../utils/config';
-// import icon_src from "./../img/arborist.png";
 
 export default class Home extends React.Component {
   public render() {
@@ -18,7 +17,7 @@ export default class Home extends React.Component {
 
           <div className="row">
             <div className="col-xs-8 col-xs-offset-2 animated zoomIn">
-              {/* <img src={icon_src} className="img-responsive center-block" /> */}
+              <img className="img-responsive center-block iconPic" />
             </div>
           </div>
 
