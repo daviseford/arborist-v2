4b99eef31e76ace6468070f715c1caa5b146141f
Davis Ford <ford_davis@bah.com>
2018-04-29 23:07:18 -0400

    update config.ts

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,4 +1,5 @@
-export const kVersion = '2.0.0';
+
+export const kVersion = require('./../package.json').version;
 export const kAppName = 'Arborist';
 export const kGitHubLink = 'https://github.com/daviseford/arborist-v2/';
 export const kBonsaiLink = 'http://360bonsai.com';
