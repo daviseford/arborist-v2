a161c54f76bb82c0eadda510a1ae57396b961604
Davis Ford <ford_davis@bah.com>
2018-04-29 22:06:04 -0400

    updated About with technical.ly article

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,3 +1,5 @@
+// tslint:disable:max-line-length
+import { shell } from 'electron';
 import * as React from 'react';
 import ButtonLinkTo from '../ui/ButtonLinkTo';
 import ButtonOpenExternal from '../ui/ButtonOpenExternal';
@@ -57,6 +59,34 @@ class Braum extends React.Component<{}, IBraumState> {
             onMouseLeave={this.asherTilt}
           />
           <h3>CEO</h3>
+          <div className="col-xs-12 text-left">
+            <p><strong>Braum Katz</strong> is a filmmaker.</p>
+
+            <p>And as a filmmaker he wanted to be able to make really beautiful 360 video — cinematic quality, really.
+            So he started to look at camera options.</p>
+
+            <p>And what he found was a little disheartening.</p>
+
+            <p>"GoPro has a lot of limitations from a filmmaker's perspective," <strong>Katz</strong> told Technical.ly.</p>
+
+            <p>And the uber-expensive old-school rigs? "It's not really practical for most people."</p>
+
+            <p>So <strong>Katz</strong>, his brother <strong>Asher</strong> and fellow filmmaker friend <strong>Charles Blatz</strong> decided they could make something to split the difference.</p>
+
+            <p><strong>And make something they did.</strong></p>
+
+            <p>The Bonsai team built a proprietary rig that holds up to four DSLR cameras.
+            Mounted with the cameras, the Excalibur can create 360 video in stunning 6k resolution.
+            As <strong>Katz</strong> put it — "Excalibur eats 4k videos for breakfast."
+              The fact that the rig uses DSLR cameras also means that filmmakers have very precise controls over lens use and camera settings.
+          </p>
+
+            <p className="text-muted text-left">-- Adapted from&nbsp;
+      <a onClick={() => shell.openExternal('https://technical.ly/dc/2017/03/02/bonsai-excalibur-360-camera/')} >
+                Technical.ly
+      </a>
+            </p>
+          </div>
         </div>
       </div>
     );
